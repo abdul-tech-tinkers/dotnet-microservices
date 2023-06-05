6b47fe855ad078c0dcaff9d88c5287c4d7935722
Jameel <abdul.jameel@siemens-healthineers.com>
2023-06-05 18:00:09 +0530

    before training2

--- CarDrivenApp/VehicleAuthService/Interface/ITokenService.cs
+++ CarDrivenApp/VehicleAuthService/Interface/ITokenService.cs
@@ -1,6 +1,9 @@
-﻿namespace VehicleAuthService.Interface
+﻿using System.Collections.Generic;
+
+namespace VehicleAuthService.Interface
 {
     public interface ITokenService
     {
+        string BuildToken(string key, string issuer, IEnumerable<string> audience, string userName);
     }
 }
--- CarDrivenApp/VehicleAuthService/Program.cs
+++ CarDrivenApp/VehicleAuthService/Program.cs
@@ -1,5 +1,7 @@
 using VehicleAuthService.Interface;
 using VehicleAuthService.Service;
+using Microsoft.AspNetCore.Authorization;
+using System.ComponentModel.DataAnnotations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddAuthorization();
 builder.Services.AddSingleton<ITokenService, TokenService>();
 
@@ -21,6 +24,32 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapPost("/validate", [AllowAnonymous] (UserValidationRequestModel request,
+    HttpContext http, ITokenService tokenService) =>
+{
+    if (request is UserValidationRequestModel { UserName: "john.doe", Password: "123456" })
+    {
+        var token = tokenService.BuildToken(builder.Configuration["Jwt:Key"],
+                                            builder.Configuration["Jwt:Issuer"],
+                                            new[]
+                                            {
+                                             builder.Configuration["Jwt:Aud1"],
+                                             builder.Configuration["Jwt:Aud2"]
+                                            }, request.UserName);
+        return new
+        {
+            Token = token,
+            IsAuthenticated = true,
+        };
+    }
+    return new
+    {
+        Token = string.Empty,
+        IsAuthenticated = false
+    };
+}).WithName("Validate");
+
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
@@ -28,3 +57,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+//Where UserValidationRequestModel is defined as
+internal record UserValidationRequestModel([Required] string UserName, [Required] string Password);
+
